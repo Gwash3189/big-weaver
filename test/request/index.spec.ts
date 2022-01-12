ad7fcc0bd3f5602aa360d642a57fbad1a847e195
Adam <adamb3189@gmail.com>
2022-01-12 11:52:49 +1100

    fix: linting

--- test/request/index.spec.ts
+++ test/request/index.spec.ts
@@ -1,9 +1,9 @@
-import { NextApiRequest } from 'next';
+import { NextApiRequest } from 'next'
 import { getBody } from '../../src/request'
 
 describe('getBody', () => {
   it('returns the body of the provided object', () => {
-    const req = { body: { name: 'name' }} as unknown as NextApiRequest
+    const req = ({ body: { name: 'name' } } as unknown) as NextApiRequest
     expect(getBody<any>(req)).toEqual(req.body)
-  });
-});
+  })
+})
