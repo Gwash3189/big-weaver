18abc34daecb5de882d1fa9731d0e3beca6207a7
Adam <adamb3189@gmail.com>
2022-02-25 21:25:41 +1100

    feat: mark next 12.0.7 as peerDep

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,4 +1,4 @@
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from 'next'
 
 export type Action = (...dependencies: any) => (req: NextApiRequest, res: NextApiResponse) => void | Promise<any>
 
