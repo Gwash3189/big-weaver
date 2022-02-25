a6684bef42c9757b3ade08980c9ad68f0e24de27
Adam <adamb3189@gmail.com>
2022-02-25 21:27:55 +1100

    fix: dont import type because prettier is silly

--- src/action/index.ts
+++ src/action/index.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextApiRequest, NextApiResponse } from 'next'
 
 export type Action = (...dependencies: any) => (req: NextApiRequest, res: NextApiResponse) => void | Promise<any>
 
