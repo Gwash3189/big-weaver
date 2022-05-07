31e3f7b32a4adc17e4448af2086f831d430d1ae8
Adam <adamb3189@gmail.com>
2022-05-08 08:15:05 +1000

    feat: add spyOn, mock and replace for Facades

---
+++ src/facade/index.ts
@@ -0,0 +1,34 @@
+import { container } from 'tsyringe'
+import { constructor } from 'tsyringe/dist/typings/types'
+
+export class Facade {
+  static create<T>(klass: constructor<T>) {
+    container.registerSingleton(`original-${klass.name}`, klass)
+    return (new Proxy(klass, {
+      get(_target, propKey, receiver) {
+        const instance = container.resolve(`original-${klass.name}`)
+        return Reflect.get(instance as Object, propKey, receiver)
+      },
+    }) as unknown) as T
+  }
+
+  replace(repalcement: constructor<Facade>) {
+    container.registerSingleton(`original-${this.constructor.name}`, repalcement as unknown as constructor<Facade>)
+  }
+
+  spyOn(methodName: string, j: typeof jest) {
+    return j.spyOn(this.constructor.prototype, methodName)
+  }
+
+  mock(methodName: string, imple: (...args: any[]) => unknown, j: typeof jest) {
+    const instance = container.resolve<Object>(`original-${this.constructor.name}`)
+    const c = class extends (instance.constructor as any) {
+      constructor(...args: any[]) {
+        super(...args)
+        this[methodName] = j.fn(imple)
+      }
+    }
+
+    this.replace(c as unknown as typeof Facade)
+  }
+}
--- test/facade.spec.ts
+++ test/facade.spec.ts
@@ -1,15 +1,16 @@
 import 'reflect-metadata'
 import { Facade } from '../src/facade'
 
-class TestClass {
-  constructor(public name: string = 'Facade') {}
 
-  hello() {
-    return `hello, ${this.name}`
-  }
-}
 
 describe('Facade', () => {
+  class TestClass {
+    constructor(public name: string = 'Facade') {}
+
+    hello() {
+      return `hello, ${this.name}`
+    }
+  }
   let klass: TestClass | null = null
 
   beforeEach(() => {
@@ -23,6 +24,75 @@ describe('Facade', () => {
 
     it('allows us to access values', () => {
       expect(klass?.name).toEqual('Facade')
+    })
+  })
+
+  describe('#replace', () => {
+    class TestFacadeImplementation extends Facade {
+      hello(number: number) {
+        return `${number}`
+      }
+    }
+    class Replacement extends Facade {
+      mock = jest.fn()
+
+      hello(...args: any[]) {
+        this.mock(...args)
+      }
+    }
+
+    const TestFacade = Facade.create(TestFacadeImplementation)
+
+    beforeEach(() => {
+      TestFacade
+        .replace(Replacement)
+
+      TestFacade.hello(123123)
+    })
+
+    afterEach(() => {
+      TestFacade.replace(TestFacadeImplementation)
+    })
+
+    it('replaces the registered facade with the provided class', () => {
+      expect(TestFacade.mock).toHaveBeenCalledWith(123123)
+    })
+  })
+
+  describe('#spyOn', () => {
+    class TestFacadeImplementation extends Facade {
+      hello(number: number) {
+        return `${number}`
+      }
+    }
+    const TestFacade = Facade.create(TestFacadeImplementation)
+    let spy: jest.SpyInstance
+
+    beforeEach(() => {
+      spy = TestFacade.spyOn('hello', jest)
+      TestFacade.hello(123123)
+    })
+
+    it('returns a valid jest spy', () => {
+      expect(spy).toHaveBeenCalledWith(123123)
+    })
+  })
+
+  describe('#mock', () => {
+    class TestFacadeImplementation extends Facade {
+      hello(number: number) {
+        return `${number}`
+      }
+    }
+    const TestFacade = Facade.create(TestFacadeImplementation)
+
+    beforeEach(() => {
+      TestFacade.mock('hello', (num) => num, jest)
+      TestFacade.hello(123123)
+    })
+
+    it('returns a valid jest spy', () => {
+      expect(TestFacade.hello).toHaveBeenCalledWith(123123)
     })
   })
 })
