4e30145dc60a2bb9130abb94928c045ee9c8059c
Adam <adamb3189@gmail.com>
2022-05-08 08:33:36 +1000

    feat: extend controllers instead of decorators

--- src/controller/controller.ts
+++ src/controller/controller.ts
@@ -1,10 +1,18 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { container } from 'tsyringe'
+import { constructor } from 'tsyringe/dist/typings/types'
 import { AfterMiddleware, BeforeMiddleware, MiddlewareProvider } from './middleware'
 import { After, Before, IController } from './types'
 
 export class Controller implements IController {
   private readonly beforeMiddleware: Array<BeforeMiddleware> = []
   private readonly afterMiddleware: Array<AfterMiddleware> = []
+
+  constructor() {
+    container.register<IController>(this.constructor as constructor<IController>, {
+      useValue: this,
+    })
+  }
 
   protected before(runner: Before) {
     const middleware = new BeforeMiddleware(runner)
--- src/controller/types.ts
+++ src/controller/types.ts
@@ -3,13 +3,13 @@ import { NextApiRequest, NextApiResponse } from 'next'
 export type ActionReturn = void | Promise<any>
 export type Action = (req: NextApiRequest, res: NextApiResponse) => ActionReturn
 export interface IController {
-  get?: Action
-  put?: Action
-  delete?: Action
-  post?: Action
-  patch?: Action
-  head?: Action
-  options?: Action
+  get: Action
+  put: Action
+  delete: Action
+  post: Action
+  patch: Action
+  head: Action
+  options: Action
 }
 export type Before = (req: NextApiRequest) => ActionReturn
 export type After = (req: NextApiRequest, res: NextApiResponse) => ActionReturn
