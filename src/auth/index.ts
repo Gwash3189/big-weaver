4acfa031275477ff84ba8b82e03a43e127877180
Adam <adamb3189@gmail.com>
2022-05-08 16:36:46 +1000

    feat: add user controller

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -5,6 +5,10 @@ class AuthImplementation extends Facade {
   async attempt(password: string, hashedPassword: string) {
     return await Hash.check(password, hashedPassword)
   }
+
+  async hash(password: string) {
+    return await Hash.make(password)
+  }
 }
 
 export const Auth = Facade.create(AuthImplementation)
---
+++ src/controllers/user.ts
@@ -0,0 +1,56 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+import { Auth } from '../auth'
+import { Controller } from '../controller'
+import { Logger } from '../logger'
+import { getBody } from '../request'
+
+
+type MinimalNewUserBody = {
+  email: string,
+  password: string
+  confirmationPassword: string
+}
+
+type MinimalNewUser = Omit<MinimalNewUserBody, 'confirmationPassword'>
+
+type MinimalUser = {
+  id: string | number,
+  email: string
+}
+
+export abstract class UserController<U> extends Controller {
+  async post(req: NextApiRequest, res: NextApiResponse) {
+    const newUserBody = getBody<MinimalNewUserBody>(req)
+
+    if (newUserBody.password === newUserBody.confirmationPassword) {
+      const hashedPassword =  await Auth.hash(newUserBody.password)
+      this.beforeUserCreation(req, res)
+      const user = await this.createUser({
+        email: newUserBody.email,
+        password: hashedPassword
+      })
+      Logger.debug({ message: 'user created' })
+      this.afterUserCreation(req, res)
+      return res.json({ data: { user } })
+    } else {
+      Logger.debug({ message: 'users passwords do not match' })
+      return this.cantCreateUser(res)
+    }
+  }
+
+  beforeUserCreation(_req: NextApiRequest, _res: NextApiResponse) {
+    return undefined
+  }
+
+  afterUserCreation(_req: NextApiRequest, _res: NextApiResponse) {
+    return undefined
+  }
+
+  protected abstract createUser(userMinimalNewUser: MinimalNewUser): Promise<U & MinimalUser | null>
+
+  private cantCreateUser(res: NextApiResponse) {
+    return res.status(500).json({
+      error: 'unable to create user'
+    })
+  }
+}
