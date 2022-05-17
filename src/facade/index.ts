a63aa31dae8322299f13d863ce87edb5ef7d2c09
Adam <adamb3189@gmail.com>
2022-05-17 16:29:54 +1000

    feat: ensure we always get the classes name when using facades

--- src/facade/index.ts
+++ src/facade/index.ts
@@ -1,14 +1,23 @@
 const MethodJar = new Map<string, Function>()
 
+const getName = (item: any) => {
+  if (typeof item === 'function') {
+    return item.name
+  }
+  if (typeof item === 'object') {
+    return item.constructor.name
+  }
+}
+
 export class Facade {
   static mock(method: string, imple: () => any) {
-    MethodJar.set(`${this.name}-${method}`, (this as { [key: string]: any })[method])
+    MethodJar.set(`${getName(this)}-${method}`, (this as { [key: string]: any })[method])
     ;(this as { [key: string]: any })[method] = imple
   }
 
   static reset(...methods: string[]) {
     methods.forEach(method => {
-      ;(this as { [key: string]: any })[method] = MethodJar.get(`${this.name}-${method}`)
+      ;(this as { [key: string]: any })[method] = MethodJar.get(`${getName(this)}-${method}`)
     })
   }
 }
