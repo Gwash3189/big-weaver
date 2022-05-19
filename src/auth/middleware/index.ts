a1fbd6a7c458db67244639849649fce8cb7c3fbf
Adam <adamb3189@gmail.com>
2022-05-19 14:57:57 +1000

    feat: add protected auth middleware

---
+++ src/auth/middleware/index.ts
@@ -0,0 +1,17 @@
+import { NextApiRequest, NextApiResponse } from "next"
+import { Auth } from ".."
+import { Facade } from "../../facade"
+
+export class Protected extends Facade {
+  static async middleware(_req: NextApiRequest, res: NextApiResponse, stop: () => void) {
+    const result = await Auth.verify()
+
+    if (result === false) {
+      stop()
+
+      res.status(403).json({ errors: [
+        'Forbidden'
+      ]})
+    }
+  }
+}
