5557d79aa1c0181ba455b2288e1fba9b1375dfb0
Adam <adamb3189@gmail.com>
2022-05-20 13:59:56 +1000

    fix: move auth config into static function

--- src/controllers/configuration.ts
+++ src/controllers/configuration.ts
@@ -12,6 +12,16 @@ export class ConfigurationController extends Controller {
     return configured
   }
 
+  static configureJWTSecret() {
+    if (!Env.exists(JWT_SECRET_KEY)) {
+      Logger.debug({ message: 'no process.env.JWT_SECRET set, defaulting to development secret' })
+      Auth.configure('secret')
+    } else {
+      Logger.debug({ message: 'configuring Auth with provided process.env.JWT_SECRET' })
+      Auth.configure(Env.get(JWT_SECRET_KEY) as string)
+    }
+  }
+
   constructor() {
     super()
 
@@ -25,15 +35,7 @@ export class ConfigurationController extends Controller {
       this.complete()
     })
 
-    this.before(() => {
-      if (!Env.exists(JWT_SECRET_KEY)) {
-        Logger.debug({ message: 'no process.env.JWT_SECRET set, defaulting to development secret' })
-        Auth.configure('secret')
-      } else {
-        Logger.debug({ message: 'configuring Auth with provided process.env.JWT_SECRET' })
-        Auth.configure(Env.get(JWT_SECRET_KEY) as string)
-      }
-    })
+    this.before(ConfigurationController.configureJWTSecret)
   }
 
   get(_request: NextApiRequest, res: NextApiResponse) {
