40b83507ab206bbca3ef8131b1ff4b6a088f15cc
Adam <adamb3189@gmail.com>
2022-05-24 16:46:30 +1000

    fix: req -> request

--- src/request/index.ts
+++ src/request/index.ts
@@ -1,15 +1,15 @@
 import { NextApiRequest } from 'next'
 
-export function getBody<T>(req: NextApiRequest) {
-  return req.body as T
+export function getBody<T>(request: NextApiRequest) {
+  return request.body as T
 }
 
-export function getQuery<T>(req: NextApiRequest) {
-  return (req.query as unknown) as T
+export function getQuery<T>(request: NextApiRequest) {
+  return (request.query as unknown) as T
 }
 
-export function getPageFromQuery(req: NextApiRequest) {
-  let page = parseInt(getQuery<{ page?: string }>(req).page || '1', 10)
+export function getPageFromQuery(request: NextApiRequest) {
+  let page = parseInt(getQuery<{ page?: string }>(request).page || '1', 10)
   if (isNaN(page) || page <= 0) {
     page = 1
   }
