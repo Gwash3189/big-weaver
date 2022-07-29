d34e22e2593355dacee26e08fc7663afc22d9a02
Adam Beck <adamb3189@gmail.com>
2022-07-29 10:31:48 +1000

    feat: add additional logging for JWT refreshing

--- src/auth/middleware/index.ts
+++ src/auth/middleware/index.ts
@@ -1,6 +1,7 @@
 import { NextApiRequest, NextApiResponse } from "next"
 import { Auth } from ".."
 import { Facade } from "../../facade"
+import { Logger } from "../../logger"
 
 class JWTJar extends Facade {
   constructor(private value: any | null) {
@@ -20,9 +21,11 @@ export const CurrentJWT = new JWTJar(null)
 
 export class Protected extends Facade {
   static async middleware(_req: NextApiRequest, res: NextApiResponse, stop: () => void) {
+    Logger.debug({ message: 'Attempting to refresh the JWT for the incoming request' })
     const result = await Auth.refresh()
 
     if (result === false) {
+      Logger.debug({ message: 'Refreshing the JWT failed. Sending 403.' })
       stop()
       CurrentJWT.set(null)
 
@@ -31,6 +34,7 @@ export class Protected extends Facade {
       ]})
     }
 
+    Logger.debug({ message: 'Successfully refreshed the JWT. Storing the decoded value.' })
     CurrentJWT.set(result)
   }
 }
