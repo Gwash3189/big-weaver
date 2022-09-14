b3a5dc72a69492ae92448c727c72daba2624a3c2
Adam Beck <adamb3189@gmail.com>
2022-09-14 16:12:28 +1000

    feat: better cookie interface

---
+++ src/cookie.ts
@@ -0,0 +1,20 @@
+import Cookies from 'cookies'
+import { Facade } from '@/facade'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { NetworkJar, RequestKey, ResponseKey } from '@/network-jar'
+
+export class Cookie extends Facade {
+  get<T>(name: string, defaultValue: T): T {
+    const request = NetworkJar.get<NextApiRequest>(RequestKey)
+    const response = NetworkJar.get<NextApiResponse>(ResponseKey)
+
+    return new Cookies(request, response).get(name) as T ?? defaultValue
+  }
+
+  set (name: string, value: any, options: Cookies.SetOption = {}): void {
+    const request = NetworkJar.get<NextApiRequest>(RequestKey)
+    const response = NetworkJar.get<NextApiResponse>(ResponseKey)
+
+    new Cookies(request, response).set(name, JSON.stringify(value), options)
+  }
+}
