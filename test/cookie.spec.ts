9a8a96d3813ee56ae7ed810d25715fd56aa6f1b5
Adam Beck <adamb3189@gmail.com>
2023-02-19 19:13:20 +1100

    chore: remove test body from todo test

--- test/cookie.spec.ts
+++ test/cookie.spec.ts
@@ -1,23 +1,13 @@
-import { Cookie } from '../src/cookie'
-
 describe('Cookie', () => {
   describe('#set', () => {
-    it.todo('stringifies the provided value', () => {
+    it.todo('stringifies the provided value')
 
-    })
-
-    it.todo('puts the value into a cookie', () => {
-
-    })
+    it.todo('puts the value into a cookie',)
   })
 
   describe('#get', () => {
-    it.todo('stringifies the provided value', () => {
+    it.todo('stringifies the provided value')
 
-    })
-
-    it.todo('puts the value into a cookie', () => {
-
-    })
+    it.todo('puts the value into a cookie')
   })
 })
